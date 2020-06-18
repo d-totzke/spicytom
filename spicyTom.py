6ba8e0e4598aa2a68ff906e0a065257d6cba16bb
dylan <drtotzke@uwaterloo.ca>
2020-06-18 11:56:36 -0400

    Update spicyTom.py

--- spicyTom.py
+++ spicyTom.py
@@ -10,18 +10,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+	spicy = '🌶️'
     if message.author == client.user:
         return
 
     if message.content.startswith('!spicy'):
         await message.channel.send('https://imgur.com/Ob2CHLe')
         await message.channel.send('Thats a spicy Tom!')
-
-@client.event
-async def on_message(message):
-	spicy = '🌶️'
-    if message.author == client.user:
-        return
 
     if message.content == spicy:
         await message.channel.send('https://imgur.com/Ob2CHLe')
